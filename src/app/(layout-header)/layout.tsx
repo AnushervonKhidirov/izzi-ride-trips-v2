c6e60adfa04831bb1fe51f10f2edeace010a4f2e
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-31 09:40:35 +0500

    alerts added

--- src/app/(layout-header)/layout.tsx
+++ src/app/(layout-header)/layout.tsx
@@ -1,13 +1,14 @@
 import type { ReactNode } from 'react';
 
-import Header from '@common/header/header';
 import { CookiesProvider } from 'next-client-cookies/server';
+import Header from '@common/header/header';
+import MainWithNotistack from '@common/main/main';
 
 const HeaderLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <CookiesProvider>
       <Header />
-      <main>{children}</main>
+      <MainWithNotistack>{children}</MainWithNotistack>
     </CookiesProvider>
   );
 };
