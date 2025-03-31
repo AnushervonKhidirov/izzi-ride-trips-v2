ee6dbe1512a783a76d25c2d606de9da0b45084f8
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-31 11:34:57 +0500

    alert provider updated

--- src/component/common/alert/provider.tsx
+++ src/component/common/alert/provider.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 import { SnackbarProvider } from 'notistack';
 import { AdditionalProps } from '@type/common.type';
 
-const AlertProvider: FC<AdditionalProps<{ id?: string }>> = ({ children, className, id }) => {
+const AlertProvider: FC<AdditionalProps> = ({ children }) => {
   return (
     <SnackbarProvider style={{ fontSize: '1em' }} anchorOrigin={{ horizontal: 'right', vertical: 'top' }}>
       {children}
