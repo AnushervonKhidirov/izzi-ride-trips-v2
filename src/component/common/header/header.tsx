ed65de57eec171d166cd1844ee3c2eddd5baa8d2
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-31 09:39:24 +0500

    updated

--- src/component/common/header/header.tsx
+++ src/component/common/header/header.tsx
@@ -1,27 +1,27 @@
-import type { TUser } from '@type/auth.type'
+import type { TUser } from '@type/user.type';
 
-import { headers } from 'next/headers'
+import { headers } from 'next/headers';
 
-import Navigation from '@common/navigation/navigation'
-import ProfileButton from '@common/profile-btn/profile-btn'
+import Navigation from '@common/navigation/navigation';
+import ProfileButton from '@common/profile-btn/profile-btn';
 
-import { Headers } from '@constant/headers'
-import { navigation } from './constant'
-import classes from './header.module.css'
+import { Headers } from '@constant/headers';
+import { navigation } from './constant';
+import classes from './header.module.css';
 
 const Header = async () => {
-    const headerList = await headers()
-    const userHeader = headerList.get(Headers.User)
-    const user = userHeader ? (JSON.parse(userHeader) as TUser) : null
+  const headerList = await headers();
+  const userHeader = headerList.get(Headers.User);
+  const user = userHeader ? (JSON.parse(userHeader) as TUser) : null;
 
-    return (
-        user && (
-            <header className={classes.header}>
-                <ProfileButton {...user} />
-                <Navigation className={classes.navigation} data={navigation} />
-            </header>
-        )
+  return (
+    user && (
+      <header className={classes.header}>
+        <ProfileButton {...user} />
+        <Navigation className={classes.navigation} data={navigation} />
+      </header>
     )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/component/common/profile-btn/profile-btn.tsx
+++ src/component/common/profile-btn/profile-btn.tsx
@@ -1,37 +1,37 @@
-import type { FC } from 'react'
-import type { TUser } from '@type/auth.type'
-import type { AdditionalProps } from '@type/common.type'
+import type { FC } from 'react';
+import type { TUser } from '@type/user.type';
+import type { AdditionalProps } from '@type/common.type';
 
-import Link from 'next/link'
-import { AccountCircle } from '@mui/icons-material'
-import Tooltip from '@common/tooltip/tooltip'
+import Link from 'next/link';
+import { AccountCircle } from '@mui/icons-material';
+import Tooltip from '@common/tooltip/tooltip';
 
-import classNames from 'classnames'
-import { Page } from '@constant/links'
-import { Color } from '@constant/colors'
-import classes from './profile-btn.module.css'
+import classNames from 'classnames';
+import { Page } from '@constant/links';
+import { Color } from '@constant/colors';
+import classes from './profile-btn.module.css';
 
 const ProfileButton: FC<AdditionalProps<TUser>> = ({ username, first_name, last_name, className }) => {
-    const name = getName()
+  const name = getName();
 
-    function getName() {
-        if (first_name && last_name) return `${first_name} ${first_name}`
-        if (first_name && !last_name) return first_name
-        if (last_name && !first_name) return last_name
-        return username
-    }
+  function getName() {
+    if (first_name && last_name) return `${first_name} ${first_name}`;
+    if (first_name && !last_name) return first_name;
+    if (last_name && !first_name) return last_name;
+    return username;
+  }
 
-    return (
-        <Tooltip title="Profile" followCursor>
-            <Link href={Page.Profile} className={classNames(classes.profile_btn, className)}>
-                <AccountCircle
-                    className={classes.profile_icon}
-                    style={{ width: '100%', height: '100%', fill: Color.Secondary }}
-                />
-                <div className={classes.user_name}>{name}</div>
-            </Link>
-        </Tooltip>
-    )
-}
+  return (
+    <Tooltip title="Profile" followCursor>
+      <Link href={Page.Profile} className={classNames(classes.profile_btn, className)}>
+        <AccountCircle
+          className={classes.profile_icon}
+          style={{ width: '100%', height: '100%', fill: Color.Secondary }}
+        />
+        <div className={classes.user_name}>{name}</div>
+      </Link>
+    </Tooltip>
+  );
+};
 
-export default ProfileButton
+export default ProfileButton;
